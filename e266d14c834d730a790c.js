3248e4aeaa6be112976c9ad2fa095505c9e6355e
Erik Grinn <grinn.erik@gmail.com>
2024-08-31 18:34:15 -0400

    Initial dist subtree commit

---
+++ e266d14c834d730a790c.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import { homeDiv, image } from "./home.js";
+import { menuDiv } from "./menu.js"
+import { aboutDiv } from "./about.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const contentDiv = document.getElementById('content')
+    const homeBtn = document.getElementById('homeBtn');
+    const menuBtn = document.getElementById('menuBtn');
+    const aboutBtn = document.getElementById('aboutBtn');
+
+    contentDiv.appendChild(homeDiv)
+    contentDiv.appendChild(image)
+    homeBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+    aboutBtn.style.boxShadow = ''
+    menuBtn.style.boxShadow = ''
+
+    homeBtn.addEventListener('click', () => {
+        homeBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+        menuBtn.style.boxShadow = ''
+        aboutBtn.style.boxShadow = ''
+
+        contentDiv.innerHTML = ''
+        contentDiv.appendChild(homeDiv)
+        contentDiv.appendChild(image)
+    })
+
+    menuBtn.addEventListener('click', () => {
+        homeBtn.style.boxShadow = ''
+        menuBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+        aboutBtn.style.boxShadow = ''
+        
+        contentDiv.innerHTML = ''
+        contentDiv.appendChild(menuDiv)
+    })
+
+    aboutBtn.addEventListener('click', () => {
+        homeBtn.style.boxShadow = ''
+        menuBtn.style.boxShadow = ''
+        aboutBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+
+        contentDiv.innerHTML = ''
+        contentDiv.appendChild(aboutDiv)
+    })
+});
