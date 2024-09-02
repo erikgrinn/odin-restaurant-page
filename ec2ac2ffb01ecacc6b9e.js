3bbd11c99fd381299dc8c3594ce5f79aa924f1f9
Erik Grinn <grinn.erik@gmail.com>
2024-09-02 11:39:23 -0400

    removed DOMContentLoaded listener

---
+++ ec2ac2ffb01ecacc6b9e.js
@@ -0,0 +1,46 @@
+import "./styles.css";
+import { homeDiv, homeImage } from "./home.js";
+import { menuDiv } from "./menu.js"
+import { aboutDiv } from "./about.js";
+
+
+const contentDiv = document.getElementById('content')
+const homeBtn = document.getElementById('homeBtn');
+const menuBtn = document.getElementById('menuBtn');
+const aboutBtn = document.getElementById('aboutBtn');
+
+// load home content by default
+contentDiv.appendChild(homeDiv)
+contentDiv.appendChild(homeImage)
+homeBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+aboutBtn.style.boxShadow = ''
+menuBtn.style.boxShadow = ''
+
+homeBtn.addEventListener('click', () => {
+    homeBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+    menuBtn.style.boxShadow = ''
+    aboutBtn.style.boxShadow = ''
+
+    contentDiv.innerHTML = ''
+    contentDiv.appendChild(homeDiv)
+    contentDiv.appendChild(homeImage)
+})
+
+menuBtn.addEventListener('click', () => {
+    homeBtn.style.boxShadow = ''
+    menuBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+    aboutBtn.style.boxShadow = ''
+    
+    contentDiv.innerHTML = ''
+    contentDiv.appendChild(menuDiv)
+})
+
+aboutBtn.addEventListener('click', () => {
+    homeBtn.style.boxShadow = ''
+    menuBtn.style.boxShadow = ''
+    aboutBtn.style.boxShadow = 'var(--bottom-white-shadow)'
+
+    contentDiv.innerHTML = ''
+    contentDiv.appendChild(aboutDiv)
+})
+
